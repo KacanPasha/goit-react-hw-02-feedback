63c94d90f42f91d146515eb61f6e41fb65e7c3cd
KacanPasha <129742576+KacanPasha@users.noreply.github.com>
2023-10-16 22:34:00 +0300

    made

---
+++ src/components/feedbackOptions/FeedbackOptions.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { Btn } from './FeedbackOptions.styled';
+
+
+export const FeedbackOptions = ({ onLeaveFeedback }) => (
+  <div className="statistic-blok-btn">
+    <Btn type="button" onClick={() => onLeaveFeedback('good')}>
+      Good
+    </Btn>
+    <Btn type="button" onClick={() => onLeaveFeedback('neutral')}>
+      Neutral
+    </Btn>
+    <Btn type="button" onClick={() => onLeaveFeedback('bad')}>
+      Bad
+    </Btn>
+  </div>
+);
---
+++ src/components/statistics/Statistics.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { Block } from './Statistics.styled';
+
+
+export const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+  <div className="statistic-blok">
+    <Block className="statistic-values">
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Positive Feedback: {positivePercentage} %</p>
+    </Block>
+  </div>
+);
+
+
